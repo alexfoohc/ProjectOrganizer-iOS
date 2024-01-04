c592b823cbfb1d31828da9b684268f3e0db6979b
Alejandro Hernandez <alex.hernandezc.21@gmail.com>
2024-01-03 23:15:52 -0600

    Add Edit mode in ProjectOverView

--- ProjectOrganizer-iOS/ProjectOrganizer-iOS/View/ProjectOverviewView.swift
+++ ProjectOrganizer-iOS/ProjectOrganizer-iOS/View/ProjectOverviewView.swift
@@ -10,12 +10,36 @@ import SwiftData
 
 struct ProjectOverviewView: View {
     @Environment(\.modelContext) private var modelContext
+    @Environment(\.editMode) private var editMode
     @Bindable var project: Project
+    @State private var textFieldDescription: String = ""
+    @State private var disabledTextField = true
+    @State private var buttonText = "Edit"
+    
     var body: some View {
-        NavigationLink(value: project) {
+        VStack(alignment: .leading, content: {
             Text(project.name)
-        }
+            TextField("Test", text: $textFieldDescription)
+                .disabled(disabledTextField)
+                .onChange(of: editMode?.wrappedValue) { oldValue, newValue in
+                    if (newValue != nil) && (newValue!.isEditing) {
+                        //Edit Button tapped
+                        disabledTextField = false
+                    } else {
+                        // Done button tapped
+                        disabledTextField = true
+                        buttonText = "Edit"
+                    }
+                }
+        })
         .navigationTitle(project.name)
+        .toolbar {
+            Button(action: {
+                disabledTextField.toggle()
+            }, label: {
+                Text(buttonText)
+            })
+        }
     }
 }
 
